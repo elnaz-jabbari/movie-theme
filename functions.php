0a77f60379d86f3635b789c06d9333394b7c68d5
Admin <jabbari.elnaz76@gmail.com>
2025-09-17 02:53:43 +0330

    Implemented homepage with movie list and pagination

--- functions.php
+++ functions.php
@@ -1,15 +1,39 @@
 <?php
 function movie_theme_enqueue_styles() {
     // استایل اصلی تم (style.css)
-    wp_enqueue_style( 'movie-style', get_stylesheet_uri() );
+    wp_enqueue_style(
+        'movie-style',
+        get_stylesheet_uri(),
+        array(),
+        filemtime(get_stylesheet_directory() . '/style.css')
+    );
 
     // Bootstrap CSS
-    wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), '5.3.2' );
+    wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/assets/css/bootstrap.rtl.min.css', array(), '5.3.2' );
 
     // Bootstrap JS (bundle شامل Popper)
     wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js', array(), '5.3.2', true );
 }
 add_action( 'wp_enqueue_scripts', 'movie_theme_enqueue_styles' );
+
+// ثبت منو در تم
+function movie_theme_register_menus() {
+    register_nav_menus(array(
+        'header-menu' => 'منوی هدر',
+    ));
+}
+add_action('after_setup_theme', 'movie_theme_register_menus');
+
+// فعال کردن قابلیت لوگو در تم
+function movie_theme_setup() {
+    add_theme_support('custom-logo', array(
+        'height'      => 60,
+        'width'       => 200,
+        'flex-height' => true,
+        'flex-width'  => true,
+    ));
+}
+add_action('after_setup_theme', 'movie_theme_setup');
 
 
 // ********************
@@ -80,3 +104,13 @@ function movie_theme_register_taxonomy() {
 add_action('init', 'movie_theme_register_taxonomy', 0);
 // فعال کردن پشتیبانی از تصویر شاخص
 add_theme_support('post-thumbnails');
+
+
+function my_frontpage_pagination_fix( $query ) {
+    if ( $query->is_main_query() && !is_admin() && is_front_page() ) {
+        $paged = get_query_var('paged') ? get_query_var('paged') : ( get_query_var('page') ? get_query_var('page') : 1 );
+        $query->set( 'paged', $paged );
+    }
+}
+add_action( 'pre_get_posts', 'my_frontpage_pagination_fix' );
+
--- header.php
+++ header.php
@@ -8,5 +8,53 @@
 </head>
 <body <?php body_class(); ?>>
 <header>
-header
+    <div class="header-main">
+        <div class="container">
+            <div class="row align-items-center py-2">
+
+                <!-- لوگو -->
+                <div class="col-1 site-logo text-end">
+                    <?php
+                    if (function_exists('the_custom_logo') && has_custom_logo()) {
+                        the_custom_logo();
+                    } else { ?>
+                        <a href="<?php echo home_url(); ?>" class="navbar-brand text-white"><h1>سایت فیلم</h1></a>
+                    <?php } ?>
+                </div>
+
+                <!-- منو -->
+                <div class="col-8 text-center">
+                    <nav>
+                        <?php
+                        wp_nav_menu(array(
+                            'theme_location' => 'header-menu',
+                            'container'      => false,
+                            'menu_class'     => 'navbar-nav d-flex flex-row justify-content-start',
+                            'link_class'     => 'nav-link text-white px-3'
+                        ));
+                        ?>
+                    </nav>
+                </div>
+
+                <!-- آیکون‌ها -->
+                <div class="col-3 d-flex justify-content-start header-icons">
+                    <a href="#" class="text-white mx-2">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon01.svg" alt="آیکون">
+                    </a>
+                    <a href="#" class="text-white mx-2">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon02.svg" alt="آیکون">
+                    </a>
+                    <a href="#" class="text-white mx-2">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon03.svg" alt="آیکون">
+                    </a>
+                    <a href="#" class="text-white mx-2">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon04.svg" alt="آیکون">
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
 </header>
+
