5ce85f1994f95a60f76ba67f560f7f31d87e3d14
Admin <jabbari.elnaz76@gmail.com>
2025-09-18 08:23:40 +0330

    resolve

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
 <?php
-<<<<<<< HEAD
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
-
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
-
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
-=======
 get_header();
 ?>
 
@@ -32,4 +14,3 @@ endif;
 
 get_footer();
 ?>
->>>>>>> 0a77f60379d86f3635b789c06d9333394b7c68d5
